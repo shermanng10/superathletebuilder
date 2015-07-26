de2ad1f720223251983fa21e1d0499f131499db3
fishermanng <sherman.ng10@gmail.com>
2015-07-26 18:37:22 -0400

    Set up sport urls

--- athletes/sport_urls.py
+++ athletes/sport_urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.sport_index, name='index'),
-	url(r'^(?P<sport_id>[0-9]+)/$', views.sport_detail, name='detail'),
+	url(r'^$', views.SportIndexView.as_view(), name='index'),
+	url(r'^(?P<pk>[0-9]+)/$', views.SportDetailView.as_view(), name='detail'),
 	url(r'^new/$', views.new_sport, name='new_sport'),
 ]
 
