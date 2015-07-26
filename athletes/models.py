8e0c6d74452bb74ad065f106c094d79484e13b08
fishermanng <sherman.ng10@gmail.com>
2015-07-26 12:27:59 -0400

    Define models and their fields

--- athletes/models.py
+++ athletes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Sport(models.Model):
+	name = models.CharField(max_length=20)
+
+class League(models.Model):
+	name = models.CharField(max_length=20)
+	sport = models.ForeignKey(Sport)
+
+class Team(models.Model):
+	name = models.CharField(max_length=20)
+	sport = models.ForeignKey(Sport)
+	league = models.ForeignKey(League)
+
+class Athlete(models.Model):
+	first_name = models.CharField(max_length=25)
+	last_name = models.CharField(max_length=25)
+	age = models.PositiveIntegerField()
+	gender = models.CharField(max_length=10)
+	website = models.URLField()
+	sport = models.ForeignKey(Sport)
+	league = models.ForeignKey(League)
+	team = models.ForeignKey(Team)
+
+
+
+
