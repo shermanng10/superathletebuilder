b48051dfd91f9ed1bb95df4f64ef9eddb1f93727
fishermanng <sherman.ng10@gmail.com>
2015-07-26 18:36:07 -0400

    Set up team urls

--- athletes/team_urls.py
+++ athletes/team_urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.team_index, name='index'),
-	url(r'^(?P<team_id>[0-9]+)/$', views.team_detail, name='detail'),
+	url(r'^$', views.TeamIndexView.as_view(), name='index'),
+	url(r'^(?P<pk>[0-9]+)/$', views.TeamDetailView.as_view(), name='detail'),
 	url(r'^new/$', views.new_team, name='new_team'),
 ]
 
