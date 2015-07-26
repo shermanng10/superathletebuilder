0780dbadec60163f77241854d964e83b1c29e65a
fishermanng <sherman.ng10@gmail.com>
2015-07-26 12:11:19 -0400

    Set up database to use postgresql

--- superathletebuilder/settings.py
+++ superathletebuilder/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'superathletebuilder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'superathletebuilder_db',
+        'USER': 'athlete',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
