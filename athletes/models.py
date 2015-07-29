37e88488fc8a4cf5e8ea7b63c7cd4b89ca17a60b
fishermanng <sherman.ng10@gmail.com>
2015-07-28 21:38:45 -0400

    Add force_bytes to models

--- athletes/models.py
+++ athletes/models.py
@@ -8,7 +8,7 @@ class Sport(models.Model):
 	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
-    return force_bytes(self.name)
+		return force_bytes(self.name)
 
 class League(models.Model):
 	name = models.CharField(max_length=20)
