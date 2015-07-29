1930fe6bb492d41da88fae2f902e3f966a12926a
fishermanng <sherman.ng10@gmail.com>
2015-07-28 21:43:19 -0400

    Fix indentation error on str method for league

--- athletes/models.py
+++ athletes/models.py
@@ -16,8 +16,8 @@ class League(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-def __str__(self):
-	return force_bytes(self.name)
+	def __str__(self):
+		return force_bytes(self.name)
 	
 class Team(models.Model):
 	name = models.CharField(max_length=20)
